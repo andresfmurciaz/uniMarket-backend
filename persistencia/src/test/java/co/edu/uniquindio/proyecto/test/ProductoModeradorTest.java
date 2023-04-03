a7bf7a1beb438fe8b9cc1e9f38452ec731d98011
andresf.murciaz <andresf.murciaz@gmail.com>
2023-04-03 15:40:28 -0500

    test 2

---
+++ persistencia/src/test/java/co/edu/uniquindio/proyecto/test/ProductoModeradorTest.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.proyecto.test;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+//se usa para que se conecto conla configuraciond de la BD
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductoModeradorTest {
+}
--- persistencia/src/test/java/co/edu/uniquindio/proyecto/test/UsuarioTest.java
+++ persistencia/src/test/java/co/edu/uniquindio/proyecto/test/UsuarioTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
 
 @DataJpaTest
+//se usa para que se conecto conla configuraciond de la BD
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioTest {
 
