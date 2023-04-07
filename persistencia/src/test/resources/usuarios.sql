66ad234f63d96a96f4f9eee94720efeec36569b3
andresf.murciaz <andresf.murciaz@gmail.com>
2023-04-07 00:07:22 -0500

    dataset usuario

--- persistencia/src/test/resources/usuarios.sql
+++ persistencia/src/test/resources/usuarios.sql
@@ -1,14 +1,14 @@
  insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
- insert into usuario values(1,"Andres Felipe","andres@gmail.com","123","chilacoa 12","3224977434");
+ insert into usuario values(2,"Andrea Sanchez","andrea@gmail.com","123","chilacoa 13","3254977434");
+ insert into usuario values(3,"Juan","juan@gmail.com","123","chilacoa 14","323977434");
+ insert into usuario values(4,"Camila","camila@gmail.com","123","chilacoa 15","3214977434");
+ insert into usuario values(5,"Juliana","juliana@gmail.com","123","chilacoa 16","3254977434");
+ insert into usuario values(6,"Carlos","carlos@gmail.com","123","chilacoa 17","3264977434");
+ insert into usuario values(7,"Lola","lola@gmail.com","123","chilacoa 18","3274977434");
+ insert into usuario values(8,"Luis","luis@gmail.com","123","chilacoa 19","3244977434");
+ insert into usuario values(9,"Raquel","Raquel@gmail.com","123","chilacoa 20","3223477434");
+ insert into usuario values(10,"Mario","mario@gmail.com","123","chilacoa 21","4224977434");
+
+
+
+
