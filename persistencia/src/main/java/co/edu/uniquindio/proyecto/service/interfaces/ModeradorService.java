d3a61e15691f27394bfe3c8a3871b6f62833466a
andresf.murciaz <andresf.murciaz@gmail.com>
2023-04-22 15:52:56 -0500

    requerimientos moderador en interfaces

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/service/interfaces/ModeradorService.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/service/interfaces/ModeradorService.java
@@ -1,4 +1,30 @@
 package co.edu.uniquindio.proyecto.service.interfaces;
 
-public interface ModeradorService {
+import co.edu.uniquindio.proyecto.entidades.Moderador;
+import co.edu.uniquindio.proyecto.entidades.Producto;
+
+import java.util.List;
+
+public interface ModeradorService
+{
+
+/*requerimientos MODERADOR
+*
+* Loguearse.
+* Autorizar o prohibir productos.
+* La página del moderador (una vez haga login) debe mostrarle la lista de todos los productos según el estado (sin revisar, autorizados y denegados).
+*
+*/
+
+    Moderador iniciarSesion(String email, String password) throws Exception;
+
+    Producto autorizarProducto() throws  Exception;
+
+    Producto prohibirProducto() throws  Exception;
+    
+    List<Producto> listaProductosEstado() throws Exception;
+
+
+
+
 }
